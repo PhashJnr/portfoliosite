fcb285ac9b5e97940a3161c73f51128e54d97f3c
PhashJnr <yungphash@gmail.com>
2024-01-19 13:11:57 +0100

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route
-          path="/"
+          index
           element={
             <HomePage
               setIsModalOpen={setIsModalOpen}
